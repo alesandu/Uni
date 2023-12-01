3f7fdd5939fb7011f816e7dd2c9564c1a22204df
ale <alesandu9@gmail.com>
2023-12-01 14:32:51 +0100

    added some exercises

--- Algoritmi/ProlemSet/GuinneSSort.py
+++ Algoritmi/ProlemSet/GuinneSSort.py
@@ -1,28 +1,26 @@
-def Paulaner(P,B):
+def GuinneSSort(P,B):
+    scambi = 0
     i = 0
-    persone = len(P)
+    persone = len(P)-1
     while(i<persone):
         if((P[i]=='M' and B[i]=='B')or(P[i]=='F' and B[i]=='A')):
             i+=1
-            continue
-        j = i
-        if(P[i]=='M'):
-            while(B[j]!='B'):
-                j+=1
-            B[j], B[j-1] = B[j-1], B[j]
         else:
-            while(B[j]!='A'):
-                j+=1
-            B[j], B[j-1] = B[j-1], B[j]
-    return B
-
-
-
-
+            j = i
+            if(P[i]=='M'):
+                while(B[j]!='B'):
+                    j+=1
+                B[j], B[j-1] = B[j-1], B[j]
+                scambi += 1
+            else:
+                while(B[j]!='A'):
+                    j+=1
+                B[j], B[j-1] = B[j-1], B[j]
+                scambi += 1
+    return (B,scambi)
 
 P = ['M','M','M','F','F','F']
 B = ['A','A','A','B','B','B']
-B = Paulaner(P,B)
+B = GuinneSSort(P,B)
 print(P)
 print(B)
-
