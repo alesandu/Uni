3945c09c6d1d7a4d8a1aa980c5f65a4474ad5e84
ale <alesandu9@gmail.com>
2024-01-08 09:17:55 +0100

    removed problemset

--- Algoritmi/ProlemSet/GuinneSSort.py
+++ Algoritmi/ProlemSet/GuinneSSort.py
@@ -1,6 +1,7 @@
 def GuinneSSort(P,B):
     scambi = 0
     i = 0
+    c=0
     persone = len(P)-1
     while(i<persone):
         if((P[i]=='M' and B[i]=='B')or(P[i]=='F' and B[i]=='A')):
@@ -10,17 +11,19 @@ def GuinneSSort(P,B):
             if(P[i]=='M'):
                 while(B[j]!='B'):
                     j+=1
+                    c+=1
                 B[j], B[j-1] = B[j-1], B[j]
                 scambi += 1
             else:
                 while(B[j]!='A'):
                     j+=1
+                    c+=1
                 B[j], B[j-1] = B[j-1], B[j]
                 scambi += 1
-    return (B,scambi)
+    return (B,scambi,c)
 
-P = ['M','M','M','F','F','F']
-B = ['A','A','A','B','B','B']
+P = ['M','M','M','M','F','F','F']
+B = ['A','B','B','B','A','A','B']
 B = GuinneSSort(P,B)
 print(P)
 print(B)
