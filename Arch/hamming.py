51e8eb015d28fe26d1ac8a8a256fab3e2d70bf64
ale <alesandu9@gmail.com>
2024-03-25 10:24:40 +0100

    update hamming py

--- Arch/hamming.py
+++ Arch/hamming.py
@@ -1,29 +1,22 @@
-# Python program to demonstrate
-# hamming code
-
-
+#programma per calcola l'hamming de simoentta tacci sua
+#(ale_sandu)
 def calcRedundantBits(m):
 
-	# Use the formula 2 ^ r >= m + r + 1
-	# to calculate the no of redundant bits.
-	# Iterate over 0 .. m and return the value
-	# that satisfies the equation
-
+	#calcolo dei bit
 	for i in range(m):
 		if(2**i >= m + i + 1):
 			return i
 
 def posRedundantBits(data, r):
 
-	# Redundancy bits are placed at the positions
-	# which correspond to the power of 2.
+	#metti i bit nella posizione giusta
 	j = 0
 	k = 1
 	m = len(data)
 	res = ''
+ 
 
-	# If position is power of 2 then insert '0'
-	# Else append the data
+	#se non è potenza di 2 metti 0
 	for i in range(1, m + r+1):
 		if(i == 2**j):
 			res = res + '0'
@@ -32,29 +25,22 @@ def posRedundantBits(data, r):
 			res = res + data[-1 * k]
 			k += 1
 
-	# The result is reversed since positions are
-	# counted backwards. (m + r+1 ... 1)
+	#posizione rigirata
 	return res[::-1]
 
 
 def calcParityBits(arr, r):
 	n = len(arr)
 
-	# For finding rth parity bit, iterate over
-	# 0 to r - 1
+	#trova il bit di parità
 	for i in range(r):
 		val = 0
 		for j in range(1, n + 1):
 
-			# If position has 1 in ith significant
-			# position then Bitwise OR the array value
-			# to find parity bit value.
 			if(j & (2**i) == (2**i)):
 				val = val ^ int(arr[-1 * j])
-				# -1 * j is given since array is reversed
 
-		# String Concatenation
-		# (0 to n - 2^r) + parity bit + (n - 2^r + 1 to n)
+		#concatena con il bit di parità
 		arr = arr[:n-(2**i)] + str(val) + arr[n-(2**i)+1:]
 	return arr
 
@@ -63,41 +49,30 @@ def detectError(arr, nr):
 	n = len(arr)
 	res = 0
 
-	# Calculate parity bits again
+	#ricalcola il bit di parità
 	for i in range(nr):
 		val = 0
 		for j in range(1, n + 1):
 			if(j & (2**i) == (2**i)):
 				val = val ^ int(arr[-1 * j])
 
-		# Create a binary no by appending
-		# parity bits together.
-
 		res = res + val*(10**i)
 
-	# Convert binary to decimal
+	#converti in decimale
 	return int(str(res), 2)
 
 
-# Enter the data to be transmitted
 data = input("Inserire seq: ")
-
-# Calculate the no of Redundant Bits Required
 m = len(data)
 r = calcRedundantBits(m)
 print(r)
-# Determine the positions of Redundant Bits
 arr = posRedundantBits(data, r)
 print(arr)
-# Determine the parity bits
 arr = calcParityBits(arr, r)
 print(arr)
-# Data to be transferred
-print("Data transferred is " + arr) 
+print("L'array con i bit inseriti è: " + arr)
+print("i bit delle diverse posizioni sono: ")
 
-# Stimulate error in transmission by changing
-# a bit value.
-# 10101001110 -> 11101001110, error in 10th position.
 i = 1
 c = 0
 x = len(arr)
@@ -107,7 +82,7 @@ while(i < x-1):
     i=2*i
     c+=1
 
-arr = input("Inserire data: ")
+arr = input("Inserire la sequenza di bit mandati dopo: ")
 arr = calcParityBits(arr, r)
 i = 1
 c = 0
