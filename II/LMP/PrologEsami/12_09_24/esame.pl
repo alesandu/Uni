94959c2fdb21358bc38f4164ef7a9b8478199224
ale <alesandu9@gmail.com>
2024-09-29 19:18:24 +0200

    minor changes

--- II/LMP/PrologEsami/12_09_24/esame.pl
+++ II/LMP/PrologEsami/12_09_24/esame.pl
@@ -7,28 +7,35 @@ lung([_|T], A):-
     lung(T,B),!,
     A is B+1. 
 
+%rende unica la lista senza tuple
+listifica([], []).
+listifica([(X, Y)|T], [X, Y|T2]) :-
+    listifica(T, T2),!.
+listifica([H|T], [H|T2]) :-
+    \+ H=(_,_),
+    listifica(T, T2),!.
+
 %fatti
 haStessaStruttura(P1, P2) :-
     \+ (P1 = (_ :- _)), %verifica non regola senno entra anche nell altro
     \+ (P2 = (_ :- _)), %uguale
     P1 =.. [_|Args1],
     P2 =.. [_|Args2],
-    lung(Args1, N1),
-    lung(Args2, N2),
-    N1 == N2.
+    lung(Args1, N),
+    lung(Args2, N).
 
 %regole
 haStessaStruttura((P1 :- Corpo1), (P2 :- Corpo2)) :-
     haStessaStruttura(P1,P2),
     haStessoCorpo(Corpo1, Corpo2).
 
-%verifica se hanno lo stesso numero di fatti
-haStessoCorpo(C1, C2) :- 
+haStessoCorpo(C1, C2) :-
     C1 =.. [_|Args1],
     C2 =.. [_|Args2],
-    lung(Args1, L1),
-    lung(Args2, L2),
-    L1 == L2,
+    listifica(Args1,Arg1),
+    listifica(Args2,Arg2),
+    lung(Arg1, L),
+    lung(Arg2, L),
     stessaStrutturaLista(Args1, Args2).
 
 %controlla accoppiamenti
@@ -36,3 +43,4 @@ stessaStrutturaLista([], []).
 stessaStrutturaLista([H1|T1], [H2|T2]) :-
     haStessaStruttura(H1, H2),
     stessaStrutturaLista(T1, T2).
+
