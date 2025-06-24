bb9177fe0fbf5c71fbe651d864ff7f5a516bd306
ale <alesandu9@gmail.com>
2025-06-24 16:29:50 +0200

    added exam pw

--- III/PW/Esami/Esame010223/server/index.js
+++ III/PW/Esami/Esame010223/server/index.js
@@ -9,11 +9,12 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
 
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"))
-listaid = []
-data.forEach(element => {
-    listaid.push(element.id)
+const data = JSON.parse(fs.readFileSync("dati.json", "utf-8"))
+
+app.get("/citations", (req, res) => {
+    res.status(200).json(data)
 });
+
 
 app.use((req, res) => {
 	res.status(404).json({ 
@@ -21,21 +22,16 @@ app.use((req, res) => {
         msg: "API not implemented" });
 });
 
-app.get("/list", (req, res) => {
-    res.status(200).json(listaid)
-});
-
-
-app.get("/pics/:id", (req, res) => {
+app.get("/citations/:id", (req, res) => {
     const id = parseInt(req.params.id)
-    const perso = data.find((el) => el.id === id)
-    if (perso) {
-        res.status(200).json(perso)
+    const trovato = data.find((el) => el.ID == id)
+    if (trovato) {
+        res.status(200).json(trovato)
     }
     else {
         res.status(404).json({ 
             status: "fail",
-            message: "Persona not found" })
+            message: "not found"})
     }
 });
 
@@ -44,6 +40,6 @@ app.listen(3000, (err) => {
         console.error(err)
     }
     else{
-        console.log("Server started on port 3000")
+        console.log("Server partito al 3000")
     }
 });
--- III/PW/Esami/Esame180722/server/index.js
+++ III/PW/Esami/Esame180722/server/index.js
@@ -11,19 +11,9 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 const data = JSON.parse(fs.readFileSync("dati.json", "utf-8"))
 
-
-app.get("/articoli", (req, res) => {
+app.get("/dati", (req, res) => {
     res.status(200).json(data)
 });
-
-app.get("/autori", (req, res) => {
-    const array = []
-    data.forEach(element => {
-        array.push(element.autore)
-    });
-    res.status(200).json(array)
-});
-
 
 app.listen(3000, (err) => {
     if(err){
