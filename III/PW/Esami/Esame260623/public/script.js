fc0315697438dedf662f08a885566de91e1c19a4
ale <alesandu9@gmail.com>
2025-06-27 16:22:28 +0200

    added PW exam

--- III/PW/Esami/Esame260623/public/script.js
+++ III/PW/Esami/Esame260623/public/script.js
@@ -1,43 +1,67 @@
 "use strict"
+"use strict"
+window.onload = async function(){
 
-async function fetchBlocco() {
-    try {
-        const res = await fetch("http://localhost:3000/list")
-        const data = await res.json()
-        return data
-    } catch (error) {
-        console.log("Error fetching data, ", error)
-        return null
+    async function fetchDati() {
+        try {
+            const res = await fetch("http://localhost:3000/list")
+            const data = await res.json()
+            return data
+        } catch (error) {
+            console.log("Error fetching data, ", error)
+            return null
+        }
     }
-}
 
-async function showBanner(el) {
-    const res = await fetch("http://localhost:3000/pics/" + el)
-    const data = await res.json()
-    const div = document.createElement("div")
-    div.innerHTML = data.desc
-    const body = document.querySelector("body")    
-    div.className = "banner"
-    body.appendChild(div)    
-}
-
-async function showBlocco(n) {
-    const blocco = await fetchBlocco()
-    const ul = document.getElementById("blocchi")
-
-    for (let index = n; index < n+4; index++) {
-        const li = document.createElement("li")
-        const a = document.createElement("a")
-        a.addEventListener("click", () => showBanner(blocco[index]))
-        a.innerHTML = blocco[index]
-        li.appendChild(a)
-        ul.appendChild(li)
+    async function fetchDesc(id) {
+        try {
+            const res = await fetch("http://localhost:3000/pics/" + id)
+            const data = await res.json()
+            return data
+        } catch (error) {
+            console.log("Error fetching data, ", error)
+            return null
+        }
     }
+
+    async function mostra(id){
+        const dati = await fetchDesc(id)
+        const overlay = document.getElementById("overlay")
+        overlay.innerHTML = dati.desc        
+        overlay.style.display = "flex"
+    }
+
+    async function carica(n){
+        const dati = await fetchDati()
+        const ul = document.getElementById("lista")
+        ul.innerHTML = ""
+        for (let index = 0; index < n; index++) {
+            const li = document.createElement("li")
+            const a = document.createElement("a")
+            a.innerHTML = dati[index]
+            li.appendChild(a)
+            li.addEventListener("click", () => mostra(dati[index]))
+            ul.appendChild(li)
+        }
+        const botton = document.getElementById("butto")
+        botton.addEventListener("click", () => carica(n+4)) 
+    }
+
+    async function main(n) {
+        const dati = await fetchDati()
+        const ul = document.getElementById("lista")
+        for (let index = 0; index < n; index++) {
+            const li = document.createElement("li")
+            const a = document.createElement("a")
+            a.innerHTML = dati[index]
+            li.appendChild(a)
+            li.addEventListener("click", () => mostra(dati[index]))
+            ul.appendChild(li)
+        }
+        const botton = document.getElementById("butto")
+        botton.addEventListener("click", () => carica(n+4))        
+    }
+
+    main(4)
 }
 
-const updateBlocchi = document.getElementById("bottone")
-updateBlocchi.addEventListener("click", () => {
-    showBlocco(4)
-})
-
-showBlocco(0)
--- III/PW/Esami/Esame280624/public/script.js
+++ III/PW/Esami/Esame280624/public/script.js
@@ -1,47 +1,45 @@
 "use strict"
+"use strict"
+window.onload = async function(){
 
-async function fetchPeople() {
-    try {
-        const res = await fetch("http://localhost:3000/persone")
-        const data = await res.json()
-        return data
-    } catch (error) {
-        console.log("Error fetching data, ", error)
-        return null
+    async function fetchDati() {
+        try {
+            const res = await fetch("http://localhost:3000/persone")
+            const data = await res.json()
+            return data
+        } catch (error) {
+            console.log("Error fetching data, ", error)
+            return null
+        }
     }
+
+    function mostra(el){
+        const testo = "Nome: " + el.nome + " " + el.cognome + ", Età: " + el.età
+        alert(testo)
+    }
+
+    function cambio(){
+        const body = document.querySelector("body")
+        body.style.backgroundColor = "#00796b"
+        const a = document.querySelectorAll("a")
+        a.forEach(el => el.style.color = "#313131")
+    }
+
+    async function main() {
+        const dati = await fetchDati()
+        const ul = document.createElement("ul")
+        dati.forEach(el => {
+            const li = document.createElement("li")
+            li.innerHTML = el.nome + " " + el.cognome
+            li.addEventListener("click", () => mostra(el))
+            ul.appendChild(li)
+        });
+        const prima = document.getElementById("prima")
+        prima.appendChild(ul)
+        const botton = document.getElementById("butto")
+        botton.addEventListener("click", () => cambio())        
+    }
+
+    main()
 }
 
-function showAlert(person) {
-    const text = "Nome: " + person.nome + " " + person.cognome + " Età: " + person.età
-    alert(text)
-}
-async function showPeople() {
-    const people = await fetchPeople()
-    const ul = document.getElementById("persone")
-    people.forEach(el => {
-        const li = document.createElement("li")
-        li.addEventListener("click", () => showAlert(el))
-        li.innerHTML = el.nome + " " + el.cognome
-        ul.appendChild(li)
-    });
-}
-
-showPeople()
-
-const updateColors = document.getElementById("bottone")
-updateColors.addEventListener("click", () => {
-
-    const body = document.querySelector("body")
-    const links = document.querySelectorAll("a")
-    body.style.backgroundColor = "#00796b"
-    links.forEach(element => {
-        element.style.color = "black"
-    });
-
-})
-
-const toggle = document.getElementById("hambu")
-toggle.addEventListener("click", () => {
-    const nav = document.getElementById("navbar")
-    nav.classList.toggle("active")
-})
