bc7c4c7fa90be53421e93ec92b4d1f168defcd99
ale <alesandu9@gmail.com>
2025-10-03 10:35:54 +0200

    change of structure

--- II/LMP/JavaEsami/09_07_24/Tipologia.java
+++
@@ -1,5 +0,0 @@
-package ImpresaSportiva;
-//13:50 14:22 14:37
-public enum Tipologia {
-	GIOCATORE, ALLENATORE, PREPARATOREATLETICO, MEDICO
-}
--- III/PW/Lezioni/Lezx1/betterlog/index.js
+++
@@ -1,6 +0,0 @@
-const moment = require('moment')
-
-const bl = function(txt){
-    console.log(moment().format('YYYY-MM-DD HH:mm:ss - ') + txt)
-}
-module.exports = bl;
